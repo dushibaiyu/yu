c2d6e009aa7bf7c5c50989c0316d51e007ef1f3b
dushibaiyu <dushibaiyu@yahoo.com>
2017-07-10 16:04:51 +0800

    SnowflkeBase

--- src/yu/algorithm/snowflkeId.d
+++ src/yu/algorithm/snowflkeId.d
@@ -6,40 +6,65 @@ import core.thread;
 
 /**
  * Twitter's Snowflke ID generate algorithm.to generate only ID
+ * 
+ * workerIdBits : 工作机器的ID所占的位数，支持不存在workID
+ * seqBits ： 计数器位数，默认： 10个位用来保存计数码
+ * twepoch :  开始的时间戳，ms。默认为 2017-3-1 0：0：0.0
 */
-enum long twepoch = 1488297600000L; //唯一时间，这是一个避免重复的随机量，自行设定不要大于当前时间戳， 默认为 2017-3-1 0：0：0.0
-enum int workerIdBits = 4; //机器码字节数。4个字节用来保存机器码
-enum long maxWorkerId = -1L ^ -1L << workerIdBits; //最大机器ID
-enum int sequenceBits = 10; //计数器字节数，10个字节用来保存计数码
-enum long maxSequence = -1L ^ -1L << sequenceBits; //一微秒内可以产生计数，如果达到该值则等到下一微妙在进行生成
-enum int workerIdShift = sequenceBits; //机器码数据左移位数，就是后面计数器占用的位数
-enum int timestampLeftShift = sequenceBits + workerIdBits; //时间戳左移动位数就是机器码和计数器总字节数
 
-final class SnowflkeID {
-    this(long macid) {
-        setMacId(macid);
+alias SnowflkeID = SnowflkeBase!(4);
+
+final class SnowflkeBase(int workerIdBits, int sequenceBits = 10, long twepoch = 1488297600000L)
+        if (sequenceBits > 0 && twepoch > 0)
+{
+    enum long maxSequence = -1L ^ -1L << sequenceBits; //一毫秒内可以产生计数，如果达到该值则等到下一微妙在进行生成
+    static if (workerIdBits > 0)
+    {
+        enum long maxWorkerId = -1L ^ -1L << workerIdBits; //最大机器ID
+        enum int timestampLeftShift = sequenceBits + workerIdBits; //时间戳左移动位数就是机器码和计数器位数
+    }
+    else
+    {
+        enum int timestampLeftShift = sequenceBits; //时间戳左移动位数就是机器码和计数器位数
     }
 
-    void setMacId(long macid)
-    in {
-        assert(macid <= maxWorkerId && macid >= 0);
-    }
-    body {
-        macId = macid;
+    static if (workerIdBits > 0)
+    {
+        this(long macid)
+        {
+            setMacId(macid);
+        }
+
+        void setMacId(long macid)
+        in
+        {
+            assert(macid <= maxWorkerId && macid >= 0);
+        }
+        body
+        {
+            macId = macid;
+        }
     }
 
-    long generate() {
+    long generate()
+    {
         long timestamp;
-        synchronized (this) {
+        synchronized (this)
+        {
             timestamp = Clock.currStdTime / 10000; // 获取毫秒数
-            if (lastTime >= timestamp) {
+            if (lastTime >= timestamp)
+            {
                 sequence += 1;
-            } else {
+            }
+            else
+            {
                 lastTime = timestamp;
                 sequence = 0;
             }
-            if (sequence > maxSequence) { // 如果生成的ID过大，就线程休眠1ms，然后重新获取
-                while (true) {
+            if (sequence > maxSequence)
+            {
+                while (true)
+                {
                     timestamp = Clock.currStdTime / 10000; // 获取毫秒数
                     if (timestamp > lastTime)
                         break;
@@ -48,12 +73,22 @@ final class SnowflkeID {
                 sequence = 0;
             }
         }
-        return (((timestamp - twepoch) << timestampLeftShift) | (macId << workerIdShift) | sequence);
+        static if (workerIdBits > 0)
+        {
+            return (((timestamp - twepoch) << timestampLeftShift) | (macId << sequenceBits)
+                    | sequence);
+        }
+        else
+        {
+            return (((timestamp - twepoch) << timestampLeftShift) | sequence);
+        }
     }
 
 protected:
-    void witeToNext() {
-        while (true) {
+    void witeToNext()
+    {
+        while (true)
+        {
             long timestamp = Clock.currStdTime / 10000; // 获取毫秒数
             if (timestamp > lastTime)
                 return;
@@ -61,18 +96,21 @@ protected:
     }
 
 private:
-    long macId;
+    static if (workerIdBits > 0)
+        long macId;
     long sequence = 0;
     long lastTime = 0;
 }
 
-unittest {
+unittest
+{
     import std.stdio;
 
     SnowflkeID sny = new SnowflkeID(0);
 
     writeln("SnowflkeID : ", Clock.currStdTime);
-    foreach (i; 0 .. 10000) {
+    foreach (i; 0 .. 10000)
+    {
         sny.generate();
     }
     writeln("SnowflkeID : end ", Clock.currStdTime);
