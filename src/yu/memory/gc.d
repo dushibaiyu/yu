9c9680cbf7988fc5e118be1640fe0d36c684e931
maozh <dushibaiyu@foxmail.com>
2023-09-14 11:00:33 +0800

    gc free

--- src/yu/memory/gc.d
+++ src/yu/memory/gc.d
@@ -4,23 +4,43 @@ import core.memory;
 
 import std.traits;
 
-@trusted:
-pragma(inline, true) void gcFree(T)(T obj) if (is(T == class) || is(T == interface)) {
-    destroy(obj);
-    GC.free(cast(void*) obj);
-}
 
-pragma(inline, true) void gcFree(T)(T* obj) {
-    static if (is(T == struct)) //NOTE: when it call in dstor, the struct's ~this will exec twice.
-        destroy((*obj));
-    GC.free(obj);
-}
+alias  gcDel = gcFree;
 
-pragma(inline, true) void gcFree(void[] obj, bool index = false) {
-    void* t = obj.ptr;
-    if (t) {
-        if (index)
-            t = GC.addrOf(t);
-        GC.free(t);
+void gcFree(T)(ref T x) @trusted
+{
+    static if (is(T == interface) || is(T == class))
+    {
+        destroy!false(x);
+        GC.free(GC.addrOf(cast(void*) x));
+        x = null;
+    }
+    else static if (is(T == U*, U))
+    {
+        static if (is(U == struct))
+        {
+            if (x){
+                destroy!false((*x));
+            }
+        }
+        GC.free(GC.addrOf(cast(void*) x));
+        x = null;
+    }
+    else static if (is(T : E[], E))
+    {
+        static if (is(E == struct))
+        {
+            foreach (ref k ; objs)
+            {
+                destroy!false(k);
+            }
+        }
+        GC.free(GC.addrOf(cast(void*) x.ptr));
+        x = null;
+    }
+    else
+    {
+        static assert(0, "It is not possible to delete: `" ~ T.stringof ~ "`");
     }
 }
+
